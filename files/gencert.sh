edd27bc312e7f8813b988dd344083bdfb0522a0f
Your Name <node2@example.com>
2023-11-02 08:35:26 +0000

    fix gen cert and start django

--- files/gencert.sh
+++
@@ -1,9 +0,0 @@
-CERTBOT_PASSWORD=$(openssl rand -base64 12)
-
-echo 'export CERTBOT_PASSWORD="'$CERTBOT_PASSWORD'"' >> ~/.bashrc
-
-openssl genrsa -des3 -passout pass:$CERTBOT_PASSWORD -out django.key 2048
-openssl req -new -key django.key -out django.csr -subj "/emailAddress=maksarestov2000@gmail.com/C=RU/O=CloudBlesKCORP" -passin pass:$CERTBOT_PASSWORD
-openssl x509 -req -days 365 -in django.csr -signkey django.key -out certificate.crt -passin pass:$CERTBOT_PASSWORD 
-
-exec /bin/bash
--- files/start_django.sh
+++ files/start_django.sh
@@ -1,8 +1,27 @@
-#!/usr/bin/expect -f
+#!/bin/bash
 
-set password $env(CERTBOT_PASSWORD)
+if [ -f "/certificate.crt" ]; then
+	set password=${CERTBOT_PASSWORD}
+	expect -c '
+	spawn python3 /node_backend/manage.py runsslserver --certificate /certificate.crt --key /django.key 0.0.0.0:8002
+	expect "Enter PEM pass phrase:"
+	send "'"$password"'\r"
+	interact
+	'
+else
+	CERTBOT_PASSWORD=$(openssl rand -base64 12)
 
-spawn python3 /node_backend/manage.py runsslserver --certificate /certificate.crt --key /django.key 0.0.0.0:8002
-expect "Enter PEM pass phrase:"
-send "$password\r"
-interact
+	echo 'export CERTBOT_PASSWORD="'$CERTBOT_PASSWORD'"' >> ~/.bashrc
+
+	openssl genrsa -des3 -passout pass:$CERTBOT_PASSWORD -out django.key 2048
+	openssl req -new -key django.key -out django.csr -subj "/emailAddress=maksarestov2000@gmail.com/C=RU/O=CloudBlesKCORP" -passin pass:$CERTBOT_PASSWORD
+	openssl x509 -req -days 365 -in django.csr -signkey django.key -out certificate.crt -passin pass:$CERTBOT_PASSWORD
+
+	expect -c '
+	spawn python3 /node_backend/manage.py runsslserver --certificate /certificate.crt --key /django.key 0.0.0.0:8002
+	expect "Enter PEM pass phrase:"
+	send "'"$CERTBOT_PASSWORD"'\r"
+	interact
+	'
+	exec /bin/bash
+fi
