b0e39f731bcbfc0f21ea06e0acf8c5ac8492b445
Your Name <you@example.com>
2023-12-15 14:01:44 +0000

    jst commit

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
+class MainUserModel(AbstractBaseUser):
+    username = models.CharField(unique=True, max_length=255)
 
 
 class UserDataModel(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.OneToOneField(MainUserModel, on_delete=models.CASCADE)
     FolderName = models.CharField(max_length=256)
     FolderParent = models.CharField(max_length=256)
     date_added = models.DateTimeField(auto_now_add=True)
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,74 +1,9 @@
 from django.http import HttpResponse
-from django.contrib import messages
-from django.views import View
-from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
-from django.views.generic import CreateView
-from django.urls import reverse_lazy
-from MainApp.models import NodeModel
-from .forms import CloudUserAuthForm, CloudUserLoginForm
-
-class RegistrationView(CreateView):
-    form_class = CloudUserAuthForm
-    template_name = 'registration.html'
-    success_url = reverse_lazy('MainApp:home')
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        username = form.cleaned_data['username']
-        password = form.cleaned_data['password1']
-
-        user = authenticate(self.request, username=username, password=password)
-
-        if user is not None:
-            node = self.min_user_quantity_domain()
-            node_domain = node.node_domain
-            node.user_quantity += 1
-            node.save()
-
-            user.node_domain = node_domain
-            user.save()
-            login(self.request, user)
-            return redirect('MainApp:profile')
-        return response
-
-    def min_user_quantity_domain(self):
-        min_user_quantity = float('inf')
-        min_user_quantity_node = None
-
-        for node in NodeModel.objects.all():
-            user_quantity = node.user_quantity
-            if user_quantity < min_user_quantity:
-                min_user_quantity = user_quantity
-                min_user_quantity_node = node
-        return min_user_quantity_node
+from MainApp.models import MainUserModel, UserDataModel
 
 
-class LoginView(View):
-    form_class = CloudUserLoginForm
-    template_name = 'login.html'
-
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('MainApp:home')
-            else:
-                form.add_error(None, 'Invalid credentials')
-        return render(request, self.template_name, {'form': form})
-
-
-@login_required
 def AddUser(request):
     if request.method == 'POST':
 
@@ -98,15 +33,3 @@ def AddUser(request):
     else:
         print('method=get')
         return HttpResponse('Invalid request method.')
-    
-
-
-@csrf_protect
-def home_render(request):
-    return render(request, 'main/home.html')
-
-
-@login_required
-def profile_render(request):
-    return render(request, 'registration/profile.html')
-
