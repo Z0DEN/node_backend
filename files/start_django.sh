b4e85e801a63cd9f8413b910f893edc1902aa99d
node2 <node2@gmail.com>
2024-01-29 16:55:13 +0000

    fix start django server

--- files/start_django.sh
+++ files/start_django.sh
@@ -6,5 +6,8 @@ else
 	python3 /node_backend/manage.py makemigrations
 	python3 /node_backend/manage.py migrate
 
-	/usr/bin/python3 /usr/local/bin/certbot certonly --standalone -n -m maksarestov2000@gmail.com -a --agree-tos -d $HOSTNAME.whoole.space
+	/usr/bin/python3 /usr/local/bin/certbot certonly --standalone -n -m maksarestov2000@gmail.com --agree-tos -d $HOSTNAME.whoole.space
+
+	python3 /node_backend/manage.py runsslserver --certificate /etc/letsencrypt/live/$HOSTNAME.whoole.space/fullchain.pem --key /etc/letsencrypt/live/$HOSTNAME.whoole.space/privkey.pem 0.0.0.0:8002
+
 fi
