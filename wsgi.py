c06883bc43811bfccb7dcc8e5144dd0dacf5aa5e
Your Name <you@example.com>
2024-03-11 13:11:49 +0000

    chng super.conf

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from daphne import server
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'node_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
